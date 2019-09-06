7cfdd3e7163353efd51735ee7367a180394f5a77
Brandon Whisnant <brandonwhisnant@Brandons-MacBook-Pro.local>
2019-09-05 23:18:16 -0400

    first commit

---
+++ src/components/Game.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-col class="game-container" cols="4">
+    <b-card>
+      <img class="game-image" :src="picture" alt>
+      <h4>{{ title }}</h4>
+      <h6>{{ platform }} - {{ releaseDate }}</h6>
+    </b-card>
+  </b-col>
+</template>
+
+
+
+<script>
+export default {
+  name: "Game",
+  props: {
+    title: String, 
+    platform: String,
+    releaseDate: String,
+    picture: String
+  }
+};
+</script>
+
+<style scoped>
+.game-image {
+  width: 200px;
+  height: 200px;
+}
+
+.game-container {
+  margin-bottom: 2%;
+  text-align: center;
+}
+</style>
---
+++ src/components/GameContainer.vue
@@ -0,0 +1,52 @@
+<template>
+  <b-container>
+    <b-row class="ml-auto mr-auto text-center">
+      <h3>Final Fantasy Games</h3>
+    </b-row>
+    <b-row>
+      <Game
+        v-for="game in games"
+        :key="game.id"
+        :title="game.title"
+        :platform="game.platform"
+        :releaseDate="game.releaseDate"
+        :picture="game.picture"
+      />
+    </b-row>
+  </b-container>
+</template>
+
+title: String, 
+    platform: String,
+    releaseDate: String,
+    picture: String
+
+<script>
+import axios from 'axios'
+import Game from './Game'
+export default {
+  name: 'GameContainer',
+  components: {
+    Game
+  },
+  data() {
+    return {
+      games: []
+    };
+  },
+  mounted: function() {
+    this.getGames();
+  },
+  methods: {
+    getGames() {
+      axios.get("https://www.moogleapi.com/api/v1/games").then(response => {
+        console.log(response.data);
+        this.games = response.data;
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
